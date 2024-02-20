0e170c9ed56c732bdcb75badd16eabd2ae963141
ericbringas <egbringas@gmail.com>
2024-02-19 21:19:50 -0800

    enum added
    added enum for drop down

--- app/models/post_workout.rb
+++ app/models/post_workout.rb
@@ -2,5 +2,5 @@ class PostWorkout < ApplicationRecord
     validates :name, presence: true
     validates :date, presence: true
     validates :duration, presence: true
-    enum :category, { swimming: 0, running: 1, basketball: 2, hiit: 3 }
+    enum :category, { swimming: 0, running: 1, basketball: 2, hiit: 3, walking: 4, hiking: 5, weightlifting: 6, walking: 7, biking: 8, zumba: 9, yoga: 10  }
 end
